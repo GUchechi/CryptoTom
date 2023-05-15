78779e9ef97b19f5a9d152344bd6891f1fd6739c
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 13:53:37 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+
 function App() {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
