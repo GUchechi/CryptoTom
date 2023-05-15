71683a35cff91b55d0fbcafe0c1446e323b64840
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 16:18:11 +0100

    cryptoapi

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -1,3 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeader = {
+  "X-RapidAPI-Key": "deec503e69msh9ce2aea4cb2b42ap152439jsnb4f0a92dd468",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeader });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
+
 // const options = {
 //     method: 'GET',
 //     url: 'https://coinranking1.p.rapidapi.com/coins',
