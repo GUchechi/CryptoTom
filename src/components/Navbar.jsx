e1aa4cf83da6e28df188f3e45031cb353ea291f7
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 13:39:55 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,13 @@ import {
   FundOutlined,
   MenuOutlined,
 } from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar />
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto Tom</Link>
         </Typography.Title>
