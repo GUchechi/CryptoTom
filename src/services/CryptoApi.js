8a19a5559d5507b293242db47552a700033514b1
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 15:59:03 +0100

    cryptoapi

---
+++ src/services/CryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'deec503e69msh9ce2aea4cb2b42ap152439jsnb4f0a92dd468',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
