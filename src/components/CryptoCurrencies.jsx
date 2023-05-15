e0d98fc35a8c82b55125f0e96e10a0244cd8e07e
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 18:13:38 +0100

    cryptoAPi

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -1,22 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import millify from 'millify';
-import { Link } from 'react-router-dom';
-import { Card, Row, Col, Input } from 'antd';
-import { useGetCryptosQuery } from '../services/CryptoApi';
+import React, { useEffect, useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/CryptoApi";
 
 const CryptoCurrencies = () => {
   const { data: cryptosList, isFetching } = useGetCryptosQuery();
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
   // const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(cryptos)
-
+  console.log(cryptos);
 
   return (
     <>
-      <Row gutter={[32, 32]} className="crypto-card-container"></Row>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link key={currency.id} to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {currency.change}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default CryptoCurrencies
+export default CryptoCurrencies;
