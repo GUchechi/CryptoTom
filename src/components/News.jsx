23d0afe2ef8f0bf628a66dde59def5307ef99009
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 20:53:10 +0100

    news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
-import moment from 'moment';
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+import { useGetCryptoNewsQuery } from "../services/CryptoNewsApi";
 
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-const News = () => {
-  return (
-    <div>
-      News
-    </div>
-  )
-}
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  console.log(cryptoNews);
+  return <div>News</div>;
+};
 
-export default News
+export default News;
