d84c44b5dd1cab23185a464f480a75d6a5acc07e
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 12:24:36 +0100

    refactoring

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Button, Menu, Typography, Avatar } from "antd";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
