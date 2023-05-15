32210e59abf5f0e659a11067fac87d97b960eb74
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 15:47:12 +0100

    Home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,6 +8,14 @@ const HomePage = () => {
   return (
     <>
         <Title level={2} className='heading'>Global Crypto Stats</Title>
+        <Row>
+            <Col span={12}><Statistic title='Total CryptoCurrencies'  value={5}/></Col>
+            <Col span={12}><Statistic title='Total Exchanges'  value={5}/></Col>
+            <Col span={12}><Statistic title='Total Market Caps'  value={5}/></Col>
+            <Col span={12}><Statistic title='Total 24 Hours Volume'  value={5}/></Col>
+            <Col span={12}><Statistic title='Total Markets'  value={5}/></Col>
+            <Col span={12}><Statistic title='Total CryptoCurrencies'  value={5}/></Col>
+        </Row>
     </>
   )
 }
