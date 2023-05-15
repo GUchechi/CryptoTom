480d27b282bebce8ed93f8718a037221630548fa
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 17:37:04 +0100

    cryptoApi

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import { cryptoApi } from "../services/CryptoApi";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
-
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -21,19 +21,28 @@ const HomePage = () => {
           <Statistic title="Total CryptoCurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={5} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.exchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Caps" value={5} />
+          <Statistic
+            title="Total Market Caps"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24 Hours Volume" value={5} />
+          <Statistic
+            title="Total 24 Hours Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={5} />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total CryptoCurrencies" value={5} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
