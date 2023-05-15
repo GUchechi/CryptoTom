8df999321ae243cb9adf213ef7273c6257d77f7c
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 17:59:31 +0100

    refactoring

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+import { useGetCryptosQuery } from '../services/CryptoApi';
 
 const CryptoCurrencies = () => {
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  // const [searchTerm, setSearchTerm] = useState('');
+
+  console.log(cryptos)
+
+
   return (
-    <div>
-      CryptoCurrencies
-    </div>
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container"></Row>
+    </>
   )
 }
 
