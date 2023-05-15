93aeb3e3d5aa0a432a04875f8f3006ca78b2312d
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 15:11:34 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+// import 'antd/dist/reset.css';
 import { Layout, Typography, Space } from "antd";
 import {
   Navbar,
@@ -28,8 +29,23 @@ function App() {
             </Routes>
           </div>
         </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Copyright © 2021
+            <Link to="/">CryptoTom Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
-export { default as Navbar} from '../components/Navbar'
+export { default as Navbar } from "../components/Navbar";
+export { default as HomePage } from "../components/HomePage";
+export { default as CryptoCurrencies } from "../components/CryptoCurrencies";
+export { default as CryptoDetails } from "../components/CryptoDetails";
+export { default as News } from "../components/News";
+export { default as Exchanges } from "./Exchanges";
