55fcc37713a6eba8e23e6fbd97557d890db97695
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 16:06:31 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "antd/dist/antd.css";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
