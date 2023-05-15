f0e99c98ca551816847454f2dcf17cb0913d85c4
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 20:50:17 +0100

    news

--- src/services/CryptoNewsApi.js
+++ src/services/CryptoNewsApi.js
@@ -7,3 +7,22 @@ const cryptoNewsHeaders = {
     "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
   },
 };
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/news";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
