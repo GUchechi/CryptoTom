4795a93e8b5d09076b6e3b640d09484f8437b415
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 12:49:12 +0100

    lineChart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,8 +3,20 @@ import { Col, Row, Typography } from "antd";
 
 const { Title } = Typography;
 
-
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(
+      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
+    );
+  }
+
   return (
     <>
       <Row className="chart-header">
