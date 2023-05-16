21c43195b33a616c04f4263473fbd83b46243b32
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 12:44:53 +0100

    lineChart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,11 +1,25 @@
-import { Line } from 'react-chartjs-2';
-import { Col, Row, Typography } from 'antd';
-
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   return (
-    <div>LineChart</div>
-  )
-}
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
 
-export default LineChart
+export default LineChart;
