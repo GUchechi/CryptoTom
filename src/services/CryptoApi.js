1f58a2e693296d68a461fad6b081f0ce75bc3152
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 12:35:57 +0100

    cryptoHistory

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -20,9 +20,14 @@ export const cryptoApi = createApi({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
     getCryptoHistory: builder.query({
-      query: ({coinId, timeperiod}) => createRequest(`/coin/${coinId}/history/${timeperiod}`),
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`/coin/${coinId}/history/${timeperiod}`),
     }),
   }),
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
