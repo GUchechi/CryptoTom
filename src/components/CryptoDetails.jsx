aa71d0c2e0ecd23735121089662b99644a27a7c5
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 05:45:17 +0100

    cryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import HTMLReactParser from 'html-react-parser';
-import { useParams } from 'react-router-dom';
-import millify from 'millify';
+import React from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
 
 const CryptoDetails = () => {
-  return (
-    <div>
-      CryptoDetails
-    </div>
-  )
-}
+  return <div>CryptoDetails</div>;
+};
 
-export default CryptoDetails
+export default CryptoDetails;
