fadfd5cd8f1b4aa9c523140be5797902d30fac55
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 05:44:44 +0100

    cryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
 
 const CryptoDetails = () => {
   return (
