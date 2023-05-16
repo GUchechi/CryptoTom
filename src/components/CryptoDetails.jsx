5179ea6cee60db0a43abad7766b06021755ab433
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 12:02:38 +0100

    cryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -137,6 +137,27 @@ const CryptoDetails = () => {
           ))}
         </Col>
       </Col>
+      <Col className="other-stats-info">
+        <Col className="coin-value-statistics-heading">
+          <Title level={3} className="coin-details-heading">
+            Other Stats Info
+          </Title>
+          <p>
+            An overview showing the statistics of {cryptoDetails.name}, such as
+            the base and quote currency, the rank, and trading volume.
+          </p>
+        </Col>
+        {genericStats.map(({ icon, title, value }) => (
+          <Col className="coin-stats">
+            <Col className="coin-stats-name">
+              <Text>{icon}</Text>
+              <Text>{title}</Text>
+            </Col>
+            <Text className="stats">{value}</Text>
+          </Col>
+        ))}
+      </Col>
+      
     </Col>
   );
 };
