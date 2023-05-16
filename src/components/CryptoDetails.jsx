8b32c66072e3977371b72c770de31bad3af376a2
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 11:17:20 +0100

    cryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -21,31 +21,90 @@ const { Option } = Select;
 const CryptoDetails = () => {
   const [timeperiod, setTimeperiod] = useState("7d");
   const { coinId } = useParams();
-  const {data, isFetching} = useGetCryptoDetailsQuery(coinId)
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
-  
-  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+  const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
   const stats = [
-    { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
-    { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-    { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
-    { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
-    { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+    {
+      title: "Price to USD",
+      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    {
+      title: "24h Volume",
+      value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${
+        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
+      }`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: "All-time-high(daily avg.)",
+      value: `$ ${
+        cryptoDetails?.allTimeHigh?.price &&
+        millify(cryptoDetails?.allTimeHigh?.price)
+      }`,
+      icon: <TrophyOutlined />,
+    },
   ];
 
   const genericStats = [
-    { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
-    { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Aprroved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-    { title: 'Total Supply', value: `$ ${cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
-    { title: 'Circulating Supply', value: `$ ${cryptoDetails?.supply?.circulating && millify(cryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails?.numberOfMarkets,
+      icon: <FundOutlined />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails?.numberOfExchanges,
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails?.supply?.confirmed ? (
+        <CheckOutlined />
+      ) : (
+        <StopOutlined />
+      ),
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.circulating &&
+        millify(cryptoDetails?.supply?.circulating)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
   ];
 
-
-  return <Col className="coin-detail-container">
-  
-  </Col>;
+  return (
+    <Col className="coin-detail-container">
+      <Col className="coin-heading-container">
+        <Title level={2} className="coin-name">
+          {data?.data?.coin.name} ({data?.data?.coin.symbol}) Price
+        </Title>
+        <p>
+          {cryptoDetails.name} live price in US Dollar (USD). View value
+          statistics, market cap and supply.
+        </p>
+      </Col>
+    </Col>
+  );
 };
 
 export default CryptoDetails;
