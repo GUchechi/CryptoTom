bbfe815a4e10bf3ebf3b4ac40e357530d7c85b33
GUchechi <godswilluchendu7@gmail.com>
2023-05-17 00:08:50 +0100

    exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,12 +1,29 @@
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+import millify from "millify";
+import { Collapse, Row, Col, Typography, Avatar } from "antd";
+import HTMLReactParser from "html-react-parser";
+import { useGetExchangesQuery } from "../services/cryptoApi";
+import Loader from "./Loader";
 
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return (
-    <div>Exchanges</div>
-  )
-}
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+  // Note: To access this endpoint you need premium plan
+  if (isFetching) return <Loader />;
 
-export default Exchanges
+  return (
+    <>
+      {" "}
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
