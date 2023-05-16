f189e7cfebc02d4c26ca02d95d86fd43841df1f5
GUchechi <godswilluchendu7@gmail.com>
2023-05-16 11:15:15 +0100

    genericStats

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -22,6 +22,7 @@ const CryptoDetails = () => {
   const [timeperiod, setTimeperiod] = useState("7d");
   const { coinId } = useParams();
   const {data, isFetching} = useGetCryptoDetailsQuery(coinId)
+  const cryptoDetails = data?.data?.coin;
   
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
