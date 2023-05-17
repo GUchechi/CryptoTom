875dc4877b570bd5e9c440dc96fc17baeae74a17
GUchechi <godswilluchendu7@gmail.com>
2023-05-17 13:04:23 +0100

    exchanges

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,4 @@ export { default as HomePage } from "../components/HomePage";
 export { default as CryptoCurrencies } from "../components/CryptoCurrencies";
 export { default as CryptoDetails } from "../components/CryptoDetails";
 export { default as News } from "../components/News";
-export { default as Exchanges } from "./Exchanges";
+
