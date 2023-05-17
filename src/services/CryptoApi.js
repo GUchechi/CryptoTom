ce05d70a3402d6b5f30f47b7c8f6c7351aa7734b
GUchechi <godswilluchendu7@gmail.com>
2023-05-17 13:01:11 +0100

    exchanges

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -16,9 +16,7 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
-    getExchanges: builder.query({
-      query: () => createRequest(`/exchanges`),
-    }),
+    
     getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
