d224263a128254c676e1979ba513b23b151058e6
GUchechi <godswilluchendu7@gmail.com>
2023-06-10 13:08:49 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ function App() {
           </Typography.Title>
           <Space>
             <Link to="/">Home</Link>
-            {/* <Link to="/exchanges"> Exchanges</Link>
-            <Link to="/news">News</Link> */}
+            <Link to="/exchanges"> Exchanges</Link>
+            <Link to="/news">News</Link>
           </Space>
         </div>
       </div>
