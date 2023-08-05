cfffd79683e3c5782dc6bdd8115f146de66c9a49
GUchechi <godswilluchendu7@gmail.com>
2023-08-05 23:16:20 +0100

    index js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
 import "antd/dist/antd.css";
-import store from "./app/store";
 import { Provider } from "react-redux";
+import store from "./app/store";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
